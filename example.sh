a56d2340c4999c719f8b5e86e493d5dca57cb645
Artur Gontijo <arturgontijo@gmail.com>
2025-05-01 17:53:34 -0300

    Better restart.sh + BLOCK_TIME=5s

--- example.sh
+++ example.sh
@@ -8,11 +8,9 @@ docker run -d \
   -p 38332:38332 \
   -p 38333:38333 \
   -v $BITCOIN_DATA_DIR:/root/.bitcoin \
-  -e BLOCK_TIME=10 \
+  -e BLOCK_TIME=5 \
   signet-local
 
 bitcoin-cli -signet -rpcwallet=miner -datadir=$BITCOIN_DATA_DIR listwallets
 bitcoin-cli -signet -rpcwallet=miner -datadir=$BITCOIN_DATA_DIR getbalance
 bitcoin-cli -signet -rpcwallet=miner -datadir=$BITCOIN_DATA_DIR scantxoutset start '[{"desc":"addr(tb1q7l0fglkc2t99lg4yeg9s54a0pdnqs6rw42d3ux)"}]' | jq .total_amount
-
-tb1q7l0fglkc2t99lg4yeg9s54a0pdnqs6rw42d3ux
--- restart.sh
+++ restart.sh
@@ -1,15 +1,23 @@
+docker build -t signet-local .
+
 docker rm -f bitcoin-signet-node
 
-rm -rf bitcoin-data
 BITCOIN_DATA_DIR=$(pwd)/bitcoin-data
 
-docker run -d \
+rm -rf $BITCOIN_DATA_DIR
+
+docker run -dti \
   --rm \
   --name bitcoin-signet-node \
   -p 38332:38332 \
   -p 38333:38333 \
   -v $BITCOIN_DATA_DIR:/root/.bitcoin \
-  -e BLOCK_TIME=10 \
+  -e BLOCK_TIME=5 \
+  --entrypoint=bash \
   signet-local
+
+docker cp bitcoin.conf bitcoin-signet-node:/root/bitcoin.conf
+
+docker exec -ti bitcoin-signet-node bash -c "/run.sh"
 
 docker logs -f bitcoin-signet-node
